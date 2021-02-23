dbe071011a91a3d214eee2e1a0784942f7e91cac
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-02-23 14:29:12 -0800

    First Build

---
+++ test/precache-manifest.404a1361849f056ae1dbb955eed9f68f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "088a294c15313ce7a4c1",
+    "url": "/lights/static/css/main.6734b38f.chunk.css"
+  },
+  {
+    "revision": "088a294c15313ce7a4c1",
+    "url": "/lights/static/js/main.088a294c.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lights/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "531457df52433afb3379",
+    "url": "/lights/static/js/runtime~main.531457df.js"
+  },
+  {
+    "revision": "2c30de1d87bb3a80117b0a28390ed903",
+    "url": "/lights/index.html"
+  }
+];
